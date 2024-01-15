f7fdd0a53a9ef2f28e622d3ea5df6975aab0271a
doorkiPr <boumarafchakib7@gmail.com>
2024-01-15 05:17:15 +0100

    Generated template files

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const webpack = require('webpack');
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const config = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+      static: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.svg$/,
+        use: 'file-loader'
+      },
+      {
+        test: /\.png$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              mimetype: 'image/png'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: 'src/index.html' }],
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/template.html',
+      filename: 'index.html',
+      title: 'to-do-list',
+      favicon: './src/media/favicon.png'
+    })
+  ]
+};
+
+module.exports = config;
